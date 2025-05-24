4be1a0ccd070e6d1a9206e30413f0af938157c13
Diego Galletti <20044195@studenti.uniupo.it>
2025-05-25 00:32:28 +0200

    Added dotnet Packages: > Supabase v1.1.1 > Supabase.Postgrest v4.1.0

--- Mobishare/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Mobishare/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9c67e54797a882535184c282b8739c57c8afb10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67d4324d2e6cdc36f547e5abe4f8e0e60ecafb93")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
